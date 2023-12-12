3faf785503d2a0ec21ba411187d87e7cd0cad3f3
QForm <mr.bogdan2003@gmail.com>
2023-12-12 19:32:19 +0600

    Add IMoveCommandStartable

---
+++ spacebattle/IMoveCommandStartable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spacebattle
+{
+    internal interface IMoveCommandStartable
+    {
+        UObject Target { get; }
+        Vector Velocity { get; }
+        Queue<ICommand> Commands { get; }
+    }
+}
