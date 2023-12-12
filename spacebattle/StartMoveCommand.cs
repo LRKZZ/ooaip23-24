42952c1059cb66bebb35800a9cff75c8ceac3106
QForm <mr.bogdan2003@gmail.com>
2023-12-12 19:32:44 +0600

    Add StartMoveCommand

---
+++ spacebattle/StartMoveCommand.cs
@@ -0,0 +1,28 @@
+﻿using Hwdtech;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spacebattle
+{
+    internal class StartMoveCommand : ICommand
+    {
+        private IMoveCommandStartable order;
+
+        StartMoveCommand(IMoveCommandStartable order)
+        {
+            this.order = order;
+        }
+
+        public void Execute()
+        {
+            //1.Устанвить скорость в таргет
+            // order.target.set_property("Velocity", order.initialVelocity)
+            //2.Создать операцию движения
+            //3.записать операцию в таргет
+            //4.Закинуть команду в очередь
+        }
+    }
+}
