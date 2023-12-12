ea240e9999bbd05253b23844762a67e49f778dde
QForm <mr.bogdan2003@gmail.com>
2023-12-12 19:31:38 +0600

    Add UObject

---
+++ spacebattle/UObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spacebattle
+{
+    internal class UObject
+    {
+        int id { get; set; }
+        string action { get; set; }
+        Dictionary<string, int> param { get; set; }
+
+        public UObject(int id, string action, Dictionary<string, int> param) 
+        {
+            this.id = id;
+            this.action = action;
+            this.param = param;
+        }
+    }
+}
