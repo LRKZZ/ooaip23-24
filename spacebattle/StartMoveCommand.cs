e67d28776971bd56888f4d8f91e1703d180d0ef2
QForm <mr.bogdan2003@gmail.com>
2023-12-20 23:36:41 +0600

    basic logic for StartCommand

---
+++ spacebattle/StartMoveCommand.cs
@@ -0,0 +1,38 @@
+﻿using Hwdtech;
+namespace spacebattle
+{
+    public class StartMoveCommand : ICommand
+    {
+        private readonly IMoveStartable _startable;
+        public StartMoveCommand(IMoveStartable startable)
+        {
+            _startable = startable;
+        }
+
+        public void Execute()
+        {
+            _startable.PropertiesOfOrder.ToList().ForEach(property => IoC.Resolve<object>(
+                "Game.IUObject.SetProperty",
+                _startable.Order,
+                property.Key,
+                property.Value
+            ));
+
+            var longCmd = IoC.Resolve<ICommand>(
+                "Game.Commands.LongMove",
+                _startable.Order
+            );
+
+            var injectCmd = IoC.Resolve<IInjectable>("Game.Commands.Inject", longCmd);
+
+            IoC.Resolve<object>(
+                "Game.IUObject.SetProperty",
+                _startable.Order,
+                "Game.Commands.Inject.LongMove",
+                injectCmd
+            );
+
+            IoC.Resolve<IQueue>("Game.Queue").Add((ICommand)injectCmd);
+        }
+    }
+}
