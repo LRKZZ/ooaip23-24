0c6cf9bd00e5f67e056d04fd37bfa14d9c5afe78
QForm <mr.bogdan2003@gmail.com>
2023-12-22 23:15:25 +0600

    add basic logic for MacroCommand

---
+++ spacebattle/MacroCommandBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Hwdtech;
+
+namespace spacebattle;
+
+public class MacroCommandBuilder
+{
+    private readonly string _dependencyName;
+    private readonly IUObject _obj;
+
+    public MacroCommandBuilder(string dependencyName, IUObject obj)
+    {
+        _dependencyName = dependencyName;
+        _obj = obj;
+    }
+
+    public List<ICommand> BuildCommands()
+    {
+        var cmds = new List<ICommand>();
+        var cmdNames = IoC.Resolve<string[]>(_dependencyName);
+
+        cmdNames.ToList().ForEach(cmd_name =>
+        {
+            cmds.Add(IoC.Resolve<ICommand>(
+                cmd_name,
+                _obj
+                ));
+        });
+
+        return cmds;
+    }
+}
