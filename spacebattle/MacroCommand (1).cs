50e1fca0e8585c3763d17650d9da33a9dae3da52
QForm <mr.bogdan2003@gmail.com>
2023-12-29 20:05:05 +0600

    fix first review check

--- spacebattle/MacroCommand (1).cs
+++ spacebattle/MacroCommand (1).cs
@@ -3,9 +3,9 @@ public class MacroCommand : ICommand
 {
     public readonly List<ICommand> _cmds;
 
-    public MacroCommand(List<ICommand>? cmds)
+    public MacroCommand(List<ICommand> cmds)
     {
-        _cmds = cmds ?? throw new Exception();
+         _cmds = cmds;
     }
 
     public void Execute()
