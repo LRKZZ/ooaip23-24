32fb0f21f1191e8bc1c32c244cf17480c064c886
QformX <mr.bogdan2003@gmail.com>
2024-04-16 15:19:47 +0600

    fix format

--- spacebattletests/StepDefinitions/StartMoveCommandStepDefinitions.cs
+++ spacebattletests/StepDefinitions/StartMoveCommandStepDefinitions.cs
@@ -26,13 +26,13 @@ namespace spacebattletests.StepDefinitions
                 "Game.IUObject.SetProperty",
                 (object[] args) =>
                 {
-                    return new Action(() => 
+                    return new Action(() =>
                     {
-                    var order = (IUObject)args[0];
-                    var key = (string)args[1];
-                    var value = args[2];
+                        var order = (IUObject)args[0];
+                        var key = (string)args[1];
+                        var value = args[2];
 
-                    order.SetProperty(key, value);
+                        order.SetProperty(key, value);
                     });
                     // var order = (IUObject)args[0];
                     // var key = (string)args[1];
