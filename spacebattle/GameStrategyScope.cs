91a7912497ae82789cf4d78158de10a3cbdd69ff
LRKZZ <sashulya1006@mail.ru>
2024-05-23 22:53:35 +0600

    edit logic

--- spacebattle/GameStrategyScope.cs
+++ spacebattle/GameStrategyScope.cs
@@ -13,10 +13,27 @@ public class GameStrategyScope : IStrategy
 
         var scope = IoC.Resolve<object>("Scopes.New", parentScope);
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", scope).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "QueuePushStrategy", (object[] args) => new QueuePushStrategy().Execute(args)).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "QueuePopStrategy", (object[] args) => new QueuePopStrategy().Execute(args)).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "RetrieveObjectFromMapStrategy", (object[] args) => new RetrieveObjectFromMapStrategy().Execute(args)).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "RetrieveQuantum", (object[] args) => (object)quantum).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "QueuePushStrategy", (object[] strategyArgs) =>
+        {
+            return new QueuePushCommand((int)strategyArgs[0], (ICommand)strategyArgs[1]);
+        }).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "QueuePopStrategy", (object[] strategyArgs) => new QueuePopStrategy().Execute(strategyArgs)).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "RetrieveObjectFromMapStrategy", (object[] strategyArgs) =>
+        {
+            var id = (int)strategyArgs[0];
+            var objectDictionary = IoC.Resolve<IDictionary<int, IUObject>>("RetrieveObjects");
+            if (!objectDictionary.TryGetValue(id, out var obj))
+            {
+                throw new Exception("IUObject with specified Id not found in dictionary.");
+            }
+
+            return obj;
+        }).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "RetrieveQuantum", (object[] strategyArgs) => (object)quantum).Execute();
 
         gameScopeMapping.Add(idGame, scope);
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", parentScope).Execute();
