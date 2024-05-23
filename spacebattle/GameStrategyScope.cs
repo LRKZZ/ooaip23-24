617c14418e83c6c4d6f20a49a09a5d8efb313ec0
LRKZZ <sashulya1006@mail.ru>
2024-05-23 20:37:39 +0600

    edit troubles with scopes

--- spacebattle/GameStrategyScope.cs
+++ spacebattle/GameStrategyScope.cs
@@ -11,15 +11,15 @@ public class GameStrategyScope : IStrategy
 
         var gameScopeMapping = IoC.Resolve<IDictionary<int, object>>("GameScopeMapping");
 
-        var scope = IoC.Resolve<object>("Scopes.CreateNew", parentScope);
-        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Update", scope).Execute();
+        var scope = IoC.Resolve<object>("Scopes.New", parentScope);
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", scope).Execute();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "QueuePushStrategy", (object[] args) => new QueuePushStrategy().Execute(args)).Execute();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "QueuePopStrategy", (object[] args) => new QueuePopStrategy().Execute(args)).Execute();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "RetrieveObjectFromMapStrategy", (object[] args) => new RetrieveObjectFromMapStrategy().Execute(args)).Execute();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "RetrieveQuantum", (object[] args) => (object)quantum).Execute();
 
         gameScopeMapping.Add(idGame, scope);
-        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Update", parentScope).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", parentScope).Execute();
 
         return gameScopeMapping;
     }
--- spacebattle/QueuePopStrategy.cs
+++ spacebattle/QueuePopStrategy.cs
@@ -5,7 +5,6 @@ public class QueuePopStrategy : IStrategy
     public object Execute(params object[] args)
     {
         var id = (int)args[0];
-        _ = (ICommand)args[1];
 
         var queue = IoC.Resolve<Queue<ICommand>>("RetrieveQueueByGameId", id);
 
