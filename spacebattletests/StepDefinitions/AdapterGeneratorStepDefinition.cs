4edbc5159ffbc548e157b67299138be6c28067e9
QForm <mr.bogdan2003@gmail.com>
2024-05-28 19:24:32 +0600

    test: add test for IRotatable

--- spacebattletests/StepDefinitions/AdapterGeneratorStepDefinition.cs
+++ spacebattletests/StepDefinitions/AdapterGeneratorStepDefinition.cs
@@ -17,7 +17,7 @@ public class AdapterGeneratorTest
     }
 
     [Fact]
-    public void SuccessGenerateAdapter()
+    public void SuccessGenerateAdapterForIMoveStartable()
     {
         var oldtype = typeof(IUObject);
         var newtype = typeof(IMoveStartable);
@@ -28,7 +28,7 @@ namespace spacebattle;
 public class IMoveStartableAdapter : IMoveStartable
 {
 	private readonly IUObject _obj;
-	publicIMoveStartableAdapter(IUObjectobj) => _obj = obj;
+	public IMoveStartableAdapter (IUObjectobj) => _obj = obj;
 
 	public IUObject Order 
 	{
@@ -45,6 +45,50 @@ public class IMoveStartableAdapter : IMoveStartable
 		{
 			{
 				return IoC.Resolve<Dictionary<string, object>>(""IUObject.Get"", _obj, ""PropertiesOfOrder"");
+			}
+		}
+	}
+}".Replace("\r", "").Replace("\n", "").Replace("\t", "").Replace(" ", "");
+        var generatedCode = new AdapterBuilder(oldtype, newtype).Build().Replace("\r", "").Replace("\n", "").Replace("\t", "").Replace(" ", "");
+
+        Assert.Equal(expectedCode, generatedCode);
+    }
+
+    [Fact]
+    public void SuccessGenerateAdapterForIRotatable()
+    {
+        var oldtype = typeof(IUObject);
+        var newtype = typeof(IRotatable);
+
+        var expectedCode = @"using Hwdtech;
+namespace spacebattle;
+
+public class IRotatableAdapter : IRotatable
+{
+	private readonly IUObject _obj;
+	public IRotatable Adapter(IUObject obj) => _obj = obj;
+
+	public Angle angle 
+	{
+		get
+		{
+			{
+				return IoC.Resolve<Angle>(""IUObject.Get"", _obj, ""angle"");
+			}
+		}
+		set
+		{
+			{
+				IoC.Resolve<ICommand>(""IUObject.Set"", _obj, ""angle"", value).Execute();
+			}
+		}
+	}
+        public Angle angleSpeed
+	{
+		get
+		{
+			{
+				return IoC.Resolve<Angle>(""IUObject.Get"", _obj, ""angleSpeed"");
 			}
 		}
 	}
