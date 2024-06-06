5fac97983bdb3a4cf336e63c6ae06b3bef43df2e
LRKZZ <sashulya1006@mail.ru>
2024-06-06 10:39:16 +0600

    added DynamicAdapterBuilder

---
+++ spacebattle/DynamicAdapterBuilder.cs
@@ -0,0 +1,40 @@
+﻿namespace SpaceBattle;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Hwdtech;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+public class DynamicAdapterCompiler
+{
+    public static Assembly CompileCode(string sourceCode)
+    {
+        var assemblyName = Path.GetRandomFileName();
+        var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+
+        var references = new List<MetadataReference>
+        {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(IoC).Assembly.Location)
+        };
+
+        var compilation = CSharpCompilation.Create(
+            assemblyName,
+            new[] { syntaxTree },
+            references,
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        using var ms = new MemoryStream();
+        var result = compilation.Emit(ms);
+
+        if (!result.Success)
+        {
+            throw new InvalidOperationException("Compilation failed");
+        }
+
+        ms.Seek(0, SeekOrigin.Begin);
+        return Assembly.Load(ms.ToArray());
+    }
+}
