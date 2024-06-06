d7f22d79f12837a70a9c48fcd2d8fa89d459ee23
LRKZZ <sashulya1006@mail.ru>
2024-06-06 10:39:04 +0600

    added AdapterCodeGeneratorStrategy

---
+++ spacebattle/AdapterCodeGeneratorStrategy.cs
@@ -0,0 +1,24 @@
+﻿namespace spacebattle;
+
+using System.Reflection;
+using SpaceBattle;
+
+public class AdapterCodeGeneratorStrategy : IStrategy
+{
+    public object Strategy(params object[] args)
+    {
+        var adapterType = (Type)args[0];
+        var targetType = (Type)args[1];
+
+        var builder = new AdapterBuilder(adapterType, targetType);
+
+        var properties = adapterType.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+        foreach (var p in properties)
+        {
+            builder.CreateProperty(p);
+        }
+
+        return builder.Build();
+    }
+}
