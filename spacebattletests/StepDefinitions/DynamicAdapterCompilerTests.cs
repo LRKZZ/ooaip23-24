0b5e0b2912852069627556edcc671925f8aedcc1
LRKZZ <sashulya1006@mail.ru>
2024-06-06 10:40:00 +0600

    added DynamicAdapterCompilerTests

---
+++ spacebattletests/StepDefinitions/DynamicAdapterCompilerTests.cs
@@ -0,0 +1,24 @@
+﻿namespace spacebattletests;
+using System;
+using SpaceBattle;
+using Xunit;
+
+public class DynamicAdapterCompilerTests
+{
+    [Fact]
+    public void EnsureDynamicCompilationSucceeds()
+    {
+        _ = new DynamicAdapterCompiler();
+        var code = @"
+            public interface ITestInterface { void TestMethod(); }
+            public class TestClass : ITestInterface {
+                public void TestMethod() {}
+            }";
+
+        var compiledAssembly = DynamicAdapterCompiler.CompileCode(code);
+        var testType = compiledAssembly.GetType("TestClass");
+        Assert.NotNull(testType);
+        var instance = Activator.CreateInstance(testType);
+        Assert.IsAssignableFrom(testType, instance);
+    }
+}
